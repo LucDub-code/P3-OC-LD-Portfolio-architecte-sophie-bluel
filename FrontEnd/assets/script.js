5e3409868f7bf8dcc909734f0c2de81ed1f8e7be
LucDub-code <lucdubcom@gmail.com>
2025-05-06 18:08:38 +0200

    Fonction de filtrage des travaux

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,6 +1,6 @@
-// Récupération des travaux via l’API et affichage dans la galerie
+// Récupération des données works de l'API
 
-const gallery = document.querySelector(".gallery");
+let works = [];
 
 fetch("http://localhost:5678/api/works")
   .then((response) => {
@@ -11,23 +11,34 @@ fetch("http://localhost:5678/api/works")
     return response.json();
   })
   .then((data) => {
-    data.forEach((work) => {
-      gallery.innerHTML += `
-      <figure>
-      <img src="${work.imageUrl}" alt="${work.title}">
-      <figcaption>${work.title}</figcaption>
-      </figure>
-      `;
-    });
+    works = data;
+    displayWorks(works);
   })
   // Gestion des erreurs réseau
   .catch((error) => {
     console.log(`Erreur lors de la récupération des données: ${error.message}`);
-    gallery.innerHTML =
-      "<p class='gallery-error'>Impossible de charger la galerie</p>";
   });
 
-// Récupération des catégories via l’API et affichage des boutons de filtre
+// Fonction d'affichage des travaux dans la galerie 
+// (utilisée dans l'appel initial et dans le filtre)
+
+const gallery = document.querySelector(".gallery");
+
+function displayWorks(list) {
+  gallery.innerHTML = "";
+  list.forEach((work) => {
+    gallery.innerHTML += `
+    <figure>
+    <img src="${work.imageUrl}" alt="${work.title}">
+    <figcaption>${work.title}</figcaption>
+    </figure>
+    `;
+  });
+}
+
+// Récupération des données categories via l’API
+// Affichage des boutons 
+// Fonction de filtrage des travaux
 
 const filters = document.querySelector(".filters");
 
@@ -39,11 +50,25 @@ fetch("http://localhost:5678/api/categories")
     return response.json();
   })
   .then((data) => {
-    filters.innerHTML = `<button data-category="all">Tous</button>`;
+    filters.innerHTML = `<button class="filter-button" data-category="all">Tous</button>`;
     data.forEach((category) => {
       filters.innerHTML += `
-      <button data-category="${category.id}">${category.name}</button>
+      <button class="filter-button" data-category="${category.id}">${category.name}</button>
       `;
+    });
+    // Filtrage des travaux par catégorie grâce au boutons
+    const buttons = document.querySelectorAll(".filter-button");
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const categoryId = button.dataset.category;
+        let filteredWorks;
+        if (categoryId === "all") {
+          filteredWorks = works;
+        } else {
+          filteredWorks = works.filter((w) => w.categoryId == categoryId);
+        }
+        displayWorks(filteredWorks);
+      });
     });
   })
   .catch((error) => {
