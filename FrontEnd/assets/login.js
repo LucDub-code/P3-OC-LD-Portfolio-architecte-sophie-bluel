c4d800ca177477f3ab735ebbc2db64252a6f1b91
LucDub-code <contact@lucasdubeau.dev>
2025-05-13 17:56:15 +0200

    Gestion de l'apparition de la fenêtre modale

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,9 +1,9 @@
-// Gestion de la connexion 
+// Gestion de la connexion
 
 const form = document.querySelector(".login-form");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
-const loginError = document.querySelector(".login-error"); 
+const loginError = document.querySelector(".login-error");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,20 +12,21 @@ form.addEventListener("submit", (event) => {
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ 
-      email: emailInput.value, 
-      password: passwordInput.value }),
+    body: JSON.stringify({
+      email: emailInput.value,
+      password: passwordInput.value,
+    }),
   })
-  .then((response) => {
-    if (response.status === 404) throw new Error("Email introuvable");
-    if (response.status === 401) throw new Error("Mot de passe incorrect");
-    return response.json();
-  })
-  .then(({ token }) => {
-    sessionStorage.setItem("token", token);
-    window.location.href = "index.html";
-  })
-  .catch((error) => {
-    loginError.textContent = error.message;
-  });
+    .then((response) => {
+      if (response.status === 404) throw new Error("Email introuvable");
+      if (response.status === 401) throw new Error("Mot de passe incorrect");
+      return response.json();
+    })
+    .then(({ token }) => {
+      sessionStorage.setItem("token", token);
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      loginError.textContent = error.message;
+    });
 });
