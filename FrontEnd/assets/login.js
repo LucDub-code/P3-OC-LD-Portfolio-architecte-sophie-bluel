09bebb341384cd45b2a23a834626b4f31daf81c1
LucDub-code <contact@lucasdubeau.dev>
2025-05-13 14:05:34 +0200

    Gestion de la connexion via page login et redirection vers page d'accueil

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,31 @@
+// Gestion de la connexion 
+
+const form = document.querySelector(".login-form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const loginError = document.querySelector(".login-error"); 
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  loginError.textContent = "";
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ 
+      email: emailInput.value, 
+      password: passwordInput.value }),
+  })
+  .then((response) => {
+    if (response.status === 404) throw new Error("Email introuvable");
+    if (response.status === 401) throw new Error("Mot de passe incorrect");
+    return response.json();
+  })
+  .then(({ token }) => {
+    localStorage.setItem("token", token);
+    window.location.href = "index.html";
+  })
+  .catch((error) => {
+    loginError.textContent = error.message;
+  });
+});
