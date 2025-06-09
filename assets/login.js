b93a6d5087b4b2b72d4dcd3dfd7f812ea9990ad1
LucDub-code <contact@lucasdubeau.dev>
2025-06-09 13:59:08 +0200

    Mise à jour des URLs API vers Render

--- assets/login.js
+++ assets/login.js
@@ -11,14 +11,17 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   loginError.textContent = "";
 
-  fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      email: emailInput.value,
-      password: passwordInput.value,
-    }),
-  })
+  fetch(
+    "https://p3-oc-ld-portfolio-architecte-sophie-bluel.onrender.com/api/users/login",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: emailInput.value,
+        password: passwordInput.value,
+      }),
+    }
+  )
     .then((response) => {
       if (response.status === 404) throw new Error("Email introuvable");
       if (response.status === 401) throw new Error("Mot de passe incorrect");
