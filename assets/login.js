778d5f912e41d8fe28f8e0ea43acea833b0de9ae
LucDub-code <lucdubcom@gmail.com>
2025-09-16 03:34:04 +0200

    Remplacement de l'url de Render par celle du nouvel hébergeur Northflank

--- assets/login.js
+++ assets/login.js
@@ -12,7 +12,7 @@ form.addEventListener("submit", (event) => {
   loginError.textContent = "";
 
   fetch(
-    "https://p3-oc-ld-portfolio-architecte-sophie.onrender.com/api/users/login",
+    "https://site--sophie-bluel--sm4nx…code.run/api/users/login",
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
